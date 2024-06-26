9ab96f2a395c82e143b589ef224ea74ae1e5c563
Yarizk <yazidrizkik@gmail.com>
2024-06-26 12:32:16 +0700

    add last catch timestamp

--- catcher/catcher.js
+++ catcher/catcher.js
@@ -56,11 +56,13 @@ class Catcher {
             if (message.content.startsWith("Congratulations")) {
                 let name = message.content.split(" ").slice(4).join(" ");
                 
-                if (this.hint[message.channel.id].total === undefined) {
-                    this.hint[message.channel.id] = { total: 0 };
+                if (!this.hint[message.channel.id]) {
+                    this.hint[message.channel.id] = { total: 0, lastCatch: Date.now() };
                 }
 
                 this.hint[message.channel.id].total += 1;
+                this.hint[message.channel.id].lastCatch = Date.now();
+
                 this.client.channels.cache.get("1253691191179083898").send(`${this.count} - ${name} at ${message.guild.name}`);
                 if (this.count == 0) this.sendStatusReport(this.client.channels.cache.get("1254029514174890004"));
             
@@ -72,7 +74,7 @@ class Catcher {
             if (command == "shiny") message.channel.send(`<@${process.env.POKE_ID}> pk --shiny`);
             if (command == "speed") message.channel.send(`<@${process.env.POKE_ID}> pk --speed 31`);
             if (command == "legend") message.channel.send(`<@${process.env.POKE_ID}> pk --legendary`);
-            if (command == "duel") message.channel.send(`<@${process.env.POKE_ID}> pk --spdiv 31 --atkiv >25 --defiv > 20 --spdefiv > 20 --hpiv > 20`);
+            if (command == "duel") message.channel.send(`<@${process.env.POKE_ID}> pk --spdiv 31 --atkiv > 25 --defiv > 20 --spdefiv > 20 --hpiv > 20`);
             if (command == "duel2") message.channel.send(`<@${process.env.POKE_ID}> pk --spdiv 31 --spatkiv > 25 --defiv > 20 --spdefiv > 20 --hpiv > 20`);
             if (command == "raids") message.channel.send(`<@${process.env.POKE_ID}> pk --defiv > 25 --spdefiv > 25 --hpiv > 25`);
             if (command == "pf") message.channel.send(`<@${process.env.POKE_ID}> profile`);
@@ -80,6 +82,19 @@ class Catcher {
             if (command == "status") this.sendStatusReport(message.channel);
             if (command == "stop all spam") this.spammers.forEach(spammer => spammer.stop());
             if (command == "start all spam") this.spammers.forEach(spammer => spammer.start());
+
+            if (command == "restart") {
+                message.content.split(" ").forEach(index => {
+                    if (index.length > 0) {
+                        const indexNumber = parseInt(index);
+                        if (indexNumber > 0 && indexNumber <= this.spammers.length) {
+                            this.spammers[indexNumber - 1].stop();
+                            this.spammers[indexNumber - 1].start();
+                        }
+                    }
+                });
+                this.sendStatusReport(message.channel);
+            }
 
             if (command == "stop") {
                 message.content.split(" ").forEach(index => {
@@ -113,7 +128,7 @@ class Catcher {
                     if (this.verificate(message.channel.id, message.author.id)) {
                         console.log("in");
                         if (message.embeds[i].title.includes("A wild pokémon has аppeаred!")) {
-                            this.hint[message.channel.id] = { count: 0, responses: [], total: this.hint[message.channel.id].total };
+                            this.hint[message.channel.id] = { count: 0, responses: [], total: this.hint[message.channel.id].total , lastCatch: this.hint[message.channel.id].lastCatch };
 
                             message.channel.sendTyping();
                             setTimeout(() => {
@@ -198,24 +213,28 @@ class Catcher {
 
     async sendStatusReport(channel) {
         let statusReport = "```";
-                statusReport += "┌───┬────────────────────┬────────┬─────────────┬───────┬─────────┐\n";
-                statusReport += "│ # │ Guild ID           │ Total  │ Pokemon     │ Level │ Status  │\n";
-                statusReport += "├───┼────────────────────┼────────┼─────────────┼───────┼─────────┤\n";
-        
-                this.spammers.forEach((spammer, index) => {
-                    const details = spammer.getDetails();
-                    let total = this.hint[spammer.catchId]?.total;
-                    console.log(total)
-                    if (total === undefined) {
-                        total = 0;
-                    }
-                    statusReport += `│ ${String(index + 1).padEnd(2)}| ${spammer.server.padEnd(19)}│ ${total.toString().padEnd(6)} │ ${details.pokemon.padEnd(11)} │ ${details.level.padEnd(6)}│ ${details.status.padEnd(7)} │\n`;
-                });
-        
-                statusReport += "└───┴────────────────────┴────────┴─────────────┴───────┴─────────┘";
-                statusReport += "```"; 
-                channel.send(statusReport);
-            }
+        statusReport += "┌───┬────────────────────┬────────┬─────────────┬──────────────┬───────┬─────────┐\n";
+        statusReport += "│ # │ Guild ID           │ Total  │ Last Catch  │ Pokemon      │ Level │ Status  │\n";
+        statusReport += "├───┼────────────────────┼────────┼─────────────┼──────────────┼───────┼─────────┤\n";
+    
+        this.spammers.forEach((spammer, index) => {
+            const details = spammer.getDetails();
+            const total = this.hint[spammer.catchId]?.total ?? 0;
+            const lastCatchTimestamp = this.hint[spammer.catchId]?.lastCatch ?? 'Never';
+            const lastCatch = lastCatchTimestamp === 'Never' ? 'Never' : this.timeSinceLastCatch(lastCatchTimestamp);
+    
+            const guild = spammer.server ?? 'Unknown Guild';
+            const pokemon = details.pokemon ?? 'Unknown';
+            const level = details.level ?? 'n/a';
+            const status = details.status ?? 'Stopped';
+    
+            statusReport += `│ ${String(index + 1).padEnd(1)} │ ${guild.padEnd(18)} │ ${total.toString().padEnd(6)} │ ${lastCatch.padEnd(11)} │ ${pokemon.padEnd(12)} │ ${level.padEnd(5)} │ ${status.padEnd(7)} │\n`;
+        });
+    
+        statusReport += "└───┴────────────────────┴────────┴─────────────┴──────────────┴───────┴─────────┘";
+        statusReport += "```"; 
+        channel.send(statusReport);
+    }
 
 
     verificate(channel, author) {
@@ -223,6 +242,29 @@ class Catcher {
             this.channel.includes(channel) &&
             author == process.env.POKE_ID
         );
+    }
+    timeSinceLastCatch(timestamp) {
+        const now = Date.now();
+        const diff = now - timestamp;
+        const seconds = Math.floor(diff / 1000);
+        const minutes = Math.floor(seconds / 60);
+        const hours = Math.floor(minutes / 60);
+        const days = Math.floor(hours / 24);
+        let result  = "";
+
+        if (days > 0) {
+            result += days + 'd '
+        } 
+        if (hours > 0) {
+            result += hours%24 + 'h '
+        } 
+        if (minutes > 0) {
+            result += minutes%60 + 'm '
+        } 
+        if (seconds >= 0) {
+            result += seconds%60 + 's '
+        }
+        return result + "ago";
     }
 
     async login() {
